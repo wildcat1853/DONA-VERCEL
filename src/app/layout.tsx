21b98b5a7df5634f1364e0336e12be9d80b5322b
ilon-trask <misha007zyt@gmail.com>
2024-09-03 18:32:39 +0200

    deleted userId form cookies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import UserIdMiddleWare from "@/share/components/UserIdMiddleWare";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <UserIdMiddleWare />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
