54414f4b1ed66c7284fa2654d1dc74648b6f07b4
ilon-trask <misha007zyt@gmail.com>
2024-09-10 15:19:36 +0200

    server deploy

--- server/env.ts
+++ server/env.ts
@@ -1,21 +1,12 @@
 import z from "zod";
 import "dotenv/config";
-import { config } from "dotenv";
-import path from 'path'
-console.log(path.resolve(process.cwd(), '../.env'))
-if (process.env.NODE_ENV == 'development') {
-  config({ path: path.resolve(process.cwd(), '../.env') })
-  config({ path: path.resolve(process.cwd(), '../.env.local') })
-}
+
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   RESEND_KEY: z.string()
 });
-console.log({
-  DATABASE_URL: process.env.DATABASE_URL,
-  RESEND_KEY: process.env.RESEND_KEY
-})
+
 export const ENV = envSchema.parse({
   DATABASE_URL: process.env.DATABASE_URL,
   RESEND_KEY: process.env.RESEND_KEY
