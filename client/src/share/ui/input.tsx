7decba9bee2c814492a6360eea2ef18d7c6c70cd
wildcat1853 <83069055+wildcat1853@users.noreply.github.com>
2024-11-03 21:53:13 +0100

    Removed input outline

--- client/src/share/ui/input.tsx
+++ client/src/share/ui/input.tsx
@@ -12,8 +12,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         className={cn(
           `flex w-full rounded-md border border-input bg-transparent border-transparent
           px-3 py-2 text-sm ring-offset-background placeholder:text-[#919191]
-          focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring 
-          focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+          focus-visible:outline-none focus-visible:ring-0 
+          focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50`,
           className
         )}
         ref={ref}
