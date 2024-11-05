88e59be6171ce242261ef101794224ecbccc0f76
wildcat1853 <83069055+wildcat1853@users.noreply.github.com>
2024-11-05 23:07:43 +0100

    Update textarea.tsx

--- client/src/share/ui/textarea.tsx
+++ client/src/share/ui/textarea.tsx
@@ -11,7 +11,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         className={cn(
           // bg-background
-          `flex w-full rounded-md border  border-input bg-transparent border-transparent
+          `flex w-full rounded-md border  border-input bg-transparent
           px-3 py-2 text-sm ring-offset-background placeholder:text-[#919191] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
           className
         )}
