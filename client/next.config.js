11215879bf999304883bfc1a0294e1d351de2d3c
wildcat1853 <83069055+wildcat1853@users.noreply.github.com>
2024-12-05 22:43:44 +0200

    commit 2

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['drizzle-orm'],
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "crypto": require.resolve("crypto-browserify"),
+    };
+    return config;
   },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
